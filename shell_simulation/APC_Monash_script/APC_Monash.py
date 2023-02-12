4d44c6a2c6939c5bc10cc02dc1dede997279c6c8
Nulan <nulan.damage@gmail.com>
2023-02-12 18:34:52 +0800

    Added a new method of turning, based on generating an arc and following it

--- shell_simulation/APC_Monash_script/APC_Monash.py
+++ shell_simulation/APC_Monash_script/APC_Monash.py
@@ -43,22 +43,26 @@ import numpy as np
 #################################################################################################################################################
 
 def main():
-    '''
+    
     # 135 degree corner turn
     Coordinate_System.travel_to(15, [-77.9, -10])
-    Coordinate_System.corner(15,5,90,[-104.5,-20],-135)
+    #Coordinate_System.corner(15,5,90,[-104.5,-20],-135)
+    Coordinate_System.pointToPointCorner(15,4,90,[-104.5,-20],-135, 30)
 
+    
     Coordinate_System.travel_to(15, [-125.8, -42.1])
-    Coordinate_System.corner(15,5,-135,[-142.2,-26.7],90)
-    '''
+    Coordinate_System.pointToPointCorner(15,4,-135,[-142.2,-26.7],90, 30)
+    #Coordinate_System.corner(15,5,-135,[-142.2,-26.7],90)
 
-    # '''
+    
     # test with APC 2022 layout
-    Coordinate_System.travel_to(15, [-77.9, -15])
-    Coordinate_System.corner(15,10,90,[-50.9,-0.8],0)
+    '''
+    Coordinate_System.travel_to(15, [-77.9, -17.6])
+    Coordinate_System.pointToPointCorner(5,10,90,[-50.9,-0.8],0)
+    #Coordinate_System.corner(15,10,90,[-50.9,-0.8],0)
     rospy.loginfo ("1")
-
-
+    '''
+    """
     Coordinate_System.travel_to(20,[-41.4,-0.7])
     Coordinate_System.travel_to(20,[-29.2,-2.5])
     Coordinate_System.travel_to(20, [-23.3, -7.6])
@@ -91,38 +95,45 @@ def main():
 
     Coordinate_System.travel_to(50,[192.6,-9.5])
     Coordinate_System.travel_to(20,[217.1,-9.9])
-    Coordinate_System.corner(10,8,0,[231,-25.4],-90)
+    Coordinate_System.pointToPointCorner(10,8,0,[231,-25.4],-90)
+    #Coordinate_System.corner(10,8,0,[231,-25.4],-90)
     rospy.loginfo ("4")
 
-
     Coordinate_System.travel_to(20,[231,-40.5])
-    Coordinate_System.corner(15,8,-90,[209.3,-58.6],180)
+    #Coordinate_System.corner(15,8,-90,[209.3,-58.6],180)
+    Coordinate_System.pointToPointCorner(15,8,-90,[209.3,-58.6],180)
     rospy.loginfo ("5")
 
-
     Coordinate_System.travel_to(20,[188,-58.6])
-    Coordinate_System.corner(15,8,180,[167.4,-80.5],-90)
+    #Coordinate_System.corner(15,8,180,[167.4,-80.5],-90)
+    Coordinate_System.pointToPointCorner(15,8,180,[167.4,-80.5],-90)
     rospy.loginfo ("6")
 				
 
-    Coordinate_System.corner(15,32,-90,[116.3,-129.2],-180)
+    #Coordinate_System.corner(15,32,-90,[116.3,-129.2],-180)
+    Coordinate_System.pointToPointCorner(15,32,-90,[116.3,-129.2],-180)
     Coordinate_System.travel_to(50,[34.7,-130.1])
     Coordinate_System.travel_to(20,[18.6,-130.4])
     rospy.loginfo ("7")
+    #'''
 
-
-    Coordinate_System.corner(15,8,180,[-6,-146.6],-90)
+    #Coordinate_System.corner(15,8,180,[-6,-146.6],-90)
+    Coordinate_System.pointToPointCorner(15,8,180,[-6,-146.6],-90)
     Coordinate_System.travel_to(20,[-5.6,-187.6])
     rospy.loginfo ("8")
 
 
-    Coordinate_System.corner(15,8,-90,[-25,-197.1],-180)
-    Coordinate_System.corner(15,32,-180,[-78.1,-149.1],90)
+    #Coordinate_System.corner(15,8,-90,[-25,-197.1],-180)
+    #Coordinate_System.corner(15,32,-180,[-78.1,-149.1],90)
+    Coordinate_System.pointToPointCorner(15,8,-90,[-25,-197.1],-180)
+    Coordinate_System.pointToPointCorner(15,30,-180,[-78.1,-149.1],90, 100)
     rospy.loginfo ("9")
 
 
-    Coordinate_System.corner(15,8,90,[-98.7,-132.6],-180)
-    Coordinate_System.corner(15,28,-180,[-145.5,-80.8],90)		
+    #Coordinate_System.corner(15,8,90,[-98.7,-132.6],-180)
+    #Coordinate_System.corner(15,28,-180,[-145.5,-80.8],90)		
+    Coordinate_System.pointToPointCorner(15,8,90,[-98.7,-132.6],-180)
+    Coordinate_System.pointToPointCorner(15,28,-180,[-145.5,-80.8],90)
     rospy.loginfo ("10")
 
 
@@ -130,9 +141,11 @@ def main():
     rospy.loginfo ("11")
 
 
-    Coordinate_System.corner(15,5,90,[-127.7,-0.3],0)	
+    #Coordinate_System.corner(15,5,90,[-127.7,-0.3],0)	
+    Coordinate_System.pointToPointCorner(15,5,90,[-127.7,-0.3],0)
     Coordinate_System.travel_to(20,[-107.6,-0.4])
-    Coordinate_System.corner(15,8,0,[-77.9,21.4],90)
+    #Coordinate_System.corner(15,8,0,[-77.9,21.4],90)
+    Coordinate_System.pointToPointCorner(15,8,0,[-77.9,21.4],90)
     rospy.loginfo ("12")
 
 
@@ -141,15 +154,19 @@ def main():
     Coordinate_System.travel_to(20,[-74.5,122.3])
 
     Coordinate_System.travel_to(20,[-74.0,162.4])
-    Coordinate_System.corner(15,18,90,[-38.6,195.0],0)
+    #Coordinate_System.corner(15,18,90,[-38.6,195.0],0)
+    Coordinate_System.pointToPointCorner(15,18,90,[-38.6,195.0],0)
     Coordinate_System.travel_to(20,[-15.2,194.3])
     rospy.loginfo ("13")
 
 
-    Coordinate_System.corner(15,8,0,[-3.1,175.0],-90)
+    #Coordinate_System.corner(15,8,0,[-3.1,175.0],-90)
+    Coordinate_System.pointToPointCorner(15,8,0,[-3.1,175.0],-90)
     Coordinate_System.travel_to(20,[-3.8,150.2])
     Coordinate_System.travel_to(50,[-6,57.8])
     rospy.loginfo ("14")
+    """
+
     # '''
     val=0
     while not rospy.is_shutdown():
