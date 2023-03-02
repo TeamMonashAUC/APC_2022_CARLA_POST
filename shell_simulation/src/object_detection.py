6863641346c18e1b68b6f5df18d90cc126e86e84
FourPawnsAttack <lucaswee626@gmail.com>
2023-03-02 22:17:26 +0800

    trying to add object_detection to roslaunch

--- shell_simulation/src/object_detection.py
+++ shell_simulation/src/object_detection.py
@@ -11,51 +11,17 @@ model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 #model.cuda() # use GPU
 bridge = CvBridge()
 
+
+
 def calculate_boxArea(x1,y1,x2,y2):
     width = x2 - x1
     height = y2 - y1
     area = width * height
     return area
 
-""" Archive
-def detect_trafficLight(img,x1,y1,x2,y2):
-    
-    crop_img = img[y1:y2,x1:x2]
-    # Convert cropped image to HSV color space
-    hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+def send_warning(state):
+    return state
 
-    # Define lower and upper HSV color ranges for green, orange, and red
-    green_lower = (40, 50, 50)
-    green_upper = (80, 255, 255)
-    yellow_lower = (20, 50, 50)
-    yellow_upper = (40, 255, 255)
-    red_lower = (0, 50, 50)
-    red_upper = (10, 255, 255)
-
-    # Create binary masks for each color
-    green_mask = cv2.inRange(hsv, green_lower, green_upper)
-    orange_mask = cv2.inRange(hsv, yellow_lower, yellow_upper)
-    red_mask = cv2.inRange(hsv, red_lower, red_upper)
-
-    # Create range of coordinates that ensure only detect center traffic light
-    if x1 > 100 and x2 < 700:
-        in_range = True
-    else:
-        in_range = False
-
-    # Check if any pixels are in each binary mask and print the color to the console
-    if cv2.countNonZero(green_mask) > 0 and in_range:
-        print("Green")
-    elif cv2.countNonZero(orange_mask) > 0 and in_range:
-        print("Yellow")
-    elif cv2.countNonZero(red_mask) > 0 and in_range:
-        print("Red")
-    else:
-        print("No traffic light detected")
-"""
-def get_warning(text):
-    print(text)
-    return True
 # test
 def show_image(img,output):
     try:
@@ -74,10 +40,6 @@ def show_image(img,output):
                 probability = probability * 100
                 colour = (255,0,0) # (B,G,R)
 
-                # check traffic light state
-                # if int(index) == 9 and probability > 50:
-                #     detect_trafficLight(img,x1,y1,x2,y2)
-
                 # if the area or person crosses threshold then warn user
                 if area > 50000 and probability > 50 or (int(index) == 0 and area > 10000):
                     colour = (0,0,255)
@@ -85,7 +47,7 @@ def show_image(img,output):
                     cv2.putText(img, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, colour, 2)
                     # Draw the bounding box on the image                 
                     cv2.rectangle(img, (x1, y1), (x2, y2), colour, 2)
-                    get_warning(text)
+                    send_warning(True)
 
                 elif probability > 50:
                     # Display the object label and probability
@@ -93,6 +55,8 @@ def show_image(img,output):
                     cv2.putText(img, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, colour, 2)
                     # Draw the bounding box on the image                 
                     cv2.rectangle(img, (x1, y1), (x2, y2), colour, 2)
+                    send_warning(True)
+
     except:
         pass
 
